1fc8ebcf7e6600d08680d4409a6ddad95dd7eb25
Tim Clark <TimClark@iot.IN.gov>
2026-07-31 12:05:11 -0400

    time inference workload for cost estimation

---
+++ ec2_scripts/setup_inference.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p /opt/dlami/nvme/code 
+cd /opt/dlami/nvme/code
+
+echo "retrieve code"
+git clone https://github.com/indiana-gio/building-extraction.git
+cd building-extraction
+uv sync
+
+echo "confirm cuda availability"
+echo $(uv run python -c "import torch; print(torch.cuda.is_available())")
+
+cd ..
+aws s3 cp s3://gsci-2026-building-footprint-057331986207-us-east-2-an/Project/RawTiles10_3inch/ Project/RawTiles10_3inch/ --recursive
+aws s3 cp s3://gsci-2026-building-footprint-057331986207-us-east-2-an/CustomModel/Results_Large_Run/checkpoints/best.pt Project/CustomModel/Results_Large_Run/checkpoints/
+
+cd building-extraction
+time -p uv run building_footprint_DeepLabV3.py infer
