12addde35358ea2cd685a8b5d8d2f36b2c2722c7
Tim Clark <TimClark@iot.IN.gov>
2026-09-11 11:42:05 -0400

    type

--- ec2_scripts/setup_inference.sh
+++ ec2_scripts/setup_inference.sh
@@ -13,7 +13,7 @@ time -p echo $(uv run python -c "import torch; print(torch.cuda.is_available())"
 
 cd ..
 
-sh ~/building-extraction/ec2_scripts/copy_tiles.sh ~/building-extraction/ec2_scripts/one-off.txt
+sh ~/code/building-extraction/ec2_scripts/copy_tiles.sh ~/code/building-extraction/ec2_scripts/one-off.txt
 aws s3 cp s3://gsci-2026-building-footprint-057331986207-us-east-2-an/CustomModel/Results_Large_Run/checkpoints/best.pt project/inference/weights/
 
 mkdir -p /opt/dlami/nvme/code/project/inference/results
